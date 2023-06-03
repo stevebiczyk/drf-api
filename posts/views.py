ad31f09238b89bf1d7d2634a4e504a423f3dc53b
stevebiczyk <stevebiczyk@gmail.com>
2023-06-03 11:44:59 +0000

    Like model and serializer challenge

--- posts/views.py
+++ posts/views.py
@@ -45,7 +45,7 @@ class PostDetail(APIView):
             return post
         except Post.DoesNotExist:
             raise Http404
-    
+
     def get(self, request, pk):
         post = self.get_object(pk)
         serializer = PostSerializer(post, context={'request': request})
@@ -53,12 +53,13 @@ class PostDetail(APIView):
 
     def put(self, request, pk):
         post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data, context={'request': request})
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
     def delete(self, request, pk):
         post = self.get_object(pk)
         post.delete()
