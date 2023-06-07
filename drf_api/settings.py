7667c1803facfaf4ac926e4811f663d3678df4a7
stevebiczyk <stevebiczyk@gmail.com>
2023-06-07 00:09:01 +0000

    final changes for Advanced React walkthrough project

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -62,7 +63,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 # ALLOWED_HOSTS = ['localhost', '8000-stevebiczyk-drfapi-yp41xsxve4j.ws-eu98.gitpod.io']
-ALLOWED_HOSTS = ['localhost', 'drf-api-sb.herokuapp.com/']
+# ALLOWED_HOSTS = ['localhost', 'drf-api-sb.herokuapp.com']
+ALLOWED_HOSTS = [
+   os.environ.get('ALLOWED_HOST'),
+   'localhost',
+]
 
 # CSRF_TRUSTED_ORIGINS = [
 #     'https://stevebiczyk-drfapi-yp41xsxve4j.ws-eu98.gitpod.io/*']
@@ -106,9 +111,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+# if 'CLIENT_ORIGIN' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#         os.environ.get('CLIENT_ORIGIN')
+#     ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
